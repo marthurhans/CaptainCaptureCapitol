413d89ea3406ef81dc4fb35ca31f9632354e68aa
Mike Hans <marthurhans@gmail.com>
2025-05-14 01:50:22 -0400

    Task 3 Start: Stable D308 baseline confirmed

--- app/src/main/java/com/mikehans/d308vacationplanner/models/Vacation.java
+++ app/src/main/java/com/mikehans/d308vacationplanner/models/Vacation.java
@@ -36,6 +36,7 @@ public class Vacation implements Serializable {
     public String getEndDate() { return endDate; }
     public void setEndDate(String endDate) { this.endDate = endDate; }
 
+    // Used for debugging and UI display of vacation summary
     @Override
     public String toString() {
         return "Vacation{" +
